f0368c9a9776e0d77b029aa54c6bf949b9365893
Theofilus Christian Kuntolukito <theofilus.kuntolukito@inticorp.tech>
2022-12-26 22:15:26 +0700

    init meca ext web

---
+++ pages/help/index.js
@@ -0,0 +1,86 @@
+export default function Help() {
+    return (
+        <div style={{display:'flex',justifyContent:'center'}}>
+            <div style={{flex:1,maxWidth:'1440px',display:'flex',flexDirection:'column'}}>
+                <h1 style={{textAlign:'left'}}>
+                    Help & Support Center
+                </h1>
+                <br/>
+                <h2>
+                    Accessibility
+                </h2>
+                <p>
+                    We are committed as a matter of our organizational policy and commercial operations to app
+                    accessibility for all users of this Meca App, including all users with disabilities.
+                </p>
+                <p>
+                    In particular, we have committed as an integral component of our ongoing app development process and
+                    policies to conformance with generally recognized and accepted guidelines and/or standards for app
+                    accessibility (the "Standards"), which are currently at least the Meca app built with Javascript.
+                </p>
+                <p>
+                    While these Standards may change and/or evolve over time, it has been and continues to be our
+                    express policy, which will be implemented with the additional guidance of a third-party
+                    accessibility expert, to devote our resources to continually enhance the design and features of this
+                    mobile app so as not to discriminate against or deny access to any disabled users of the app.
+                </p>
+                <p>
+                    As part of our policy and ongoing commitment to app accessibility, we are making available this
+                    accessibility statement and the dedicated accessibility email address below in order to address any
+                    accessibility issues that individual users of this Meca app may encounter and for which they seek a
+                    reasonable accommodation or auxiliary aid.
+                </p>
+                <h2>
+                    Measures to support accessibility
+                </h2>
+                <p>
+                    We are taking the following measures to ensure accessibility of this Meca app:
+                </p>
+                <ul>
+                    <li>Include accessibility as a requirement for all mobile app content consistent with current
+                        Standards.
+                    </li>
+                    <li>Assign clear accessibility goals and responsibilities.</li>
+                    <li>Ensure content authors have accessibility knowledge and skills.</li>
+                    <li>Include accessibility as part of our mission.</li>
+                    <li>Include accessibility throughout our internal policies.</li>
+                    <li>Usability test for participants with disabilities.</li>
+                </ul>
+                <h2>
+                    If you have trouble accesing feature
+                </h2>
+                <p>
+                    If you have a disability and believe that the format of any material on this app interferes with
+                    your ability to access the information, please contact our dedicated accessibility email address:
+                    support@meca-bengkel.online
+                </p>
+                <p>To enable us to respond in a manner most helpful to you, please notify us in the first instance by
+                    emailing the following information to support@meca-bengkel.online
+                    the nature of the accessibility problem you experienced,
+                </p>
+                <ul>
+                    <li>the app problems of the requested material you tried to access,</li>
+                    <li>your preferred format in which you want to receive the material (electronic format (ASCII,
+                        etc.), standard print, large print, etc.), and
+                    </li>
+                    <li>your contact information so that we can work with you to address any accessibility issues you
+                        may have experienced.
+                    </li>
+                </ul>
+                <p>
+                    In particular, if you have difficulty accessing information on this site, particularly if using
+                    assistive technology, including screen readers, eye tracking devices, or voice recognition software,
+                    please contact us.
+                </p>
+                <p>
+                    We will get back to you as soon as we can in order to address the requests and to provide the
+                    information you are seeking.
+                </p>
+                <h2>
+                    Need more info or ready to enroll?
+                </h2>
+            </div>
+        </div>
+    )
+}
+
